652f3b9082d20df1830628ac12fa87ef0e6c0233
Avishkar <gaikwadavishkar546@gmail.com>
2024-04-07 23:41:12 +0530

    All use forgot passwor and reset password are created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,10 @@
-import UserModel from '../models/userModel.js';
 import Jwt from "jsonwebtoken";
 import { compare, hashing } from '../utils/passwordHashHandler.js';
 import { asyncHandler } from './../utils/asyncHandler.js';
 import errorHandler from '../utils/errorHandler.js';
+import crypto from 'crypto';
+import { sendEmail } from '../utils/sendEmail.js';
+import courseModel from '../models/courseModel.js';
 import userModel from '../models/userModel.js';
 
 export const registerController = asyncHandler(async (req, res, next) => {
@@ -87,8 +89,121 @@ export const getProfileController = asyncHandler(async (req, res, next) => {
     const user = await userModel.findById(req.user._id);
 
     res.status(200).send({
-        success:true,
-        message:"Profile details",
+        success: true,
+        message: "Profile details",
         user
     })
+})
+
+export const changePasswordController = asyncHandler(async (req, res, next) => {
+    const { oldPassword, newPassword } = req.body;
+    // If empty
+    if (!oldPassword || !newPassword) {
+        next(new errorHandler("incomplete credentials", 401));
+    }
+
+    let user = await userModel.findById(req.user._id);
+    const validUser = await compare(oldPassword, user.password);
+
+    if (!validUser) {
+        next(new errorHandler("Invalid credentials", 401));
+    }
+
+    const hashedPassword = await hashing(newPassword);
+    user.password = hashedPassword;
+
+    await user.save();
+    res.status(201).json({
+        success: true,
+        message: "Password changed succesfully",
+        user
+    })
+})
+
+export const updateProfileController = asyncHandler(async (req, res, next) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) return next(new errorHandler("Invalid credentials", 401));
+
+    let user = await userModel.findById(req.user._id);
+
+    user.name = name;
+    user.email = email;
+    await user.save();
+
+    res.status(201).json({
+        success: true,
+        message: "Information updated succesfully"
+    })
+
+})
+
+export const updateProfileImageController = asyncHandler(async (req, res, next) => {
+
+
+    res.status(201).send({
+        success: true,
+        message: "Profile image updated succesfully"
+    })
+})
+
+export const forgotPasswordController = asyncHandler(async (req, res, next) => {
+    const { email } = req.body;
+
+    const user = await userModel.find({ email });
+    if (!user) return next(new errorHandler("User does not exist", 401));
+
+    let resetToken = crypto.randomBytes(20).toString();
+    user.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    user.resetPasswordTokenExpire = Date.now() + 15 * 60 * 60 * 1000;
+
+    const url = `${process.env.FRONTEND_URL}/resettoken/${resetToken}`;
+    const message = `Click on this link to change password ${url}`;
+
+    sendEmail(message, user.email);
+
+    res.status(200).json({
+        success: true,
+        message: `reset token is sent to your this ${email}`,
+        data
+    });
+})
+
+export const addToPlaylist = asyncHandler(async (req, res, next) => {
+    const user = await userModel.findById(req.user._id);
+
+    const course = await courseModel.findById(req.body.id);
+    if (!course) return next(new errorHandler("Course does not exist", 404));
+
+    user.playlist.push({
+        course: course._id,
+        poster: course.poster.url
+    });
+
+    await user.save();
+    res.status(200).json({
+        success: true,
+        message: `Added to playlsit`,
+        user
+    });
+})
+
+export const removeFromPlaylist = asyncHandler(async (req, res, next) => {
+    const user = await userModel.findById(req.user._id);
+
+    const course = await courseModel.findById(req.body.id);
+    if (!course) return next(new errorHandler("Course does not exist", 404));
+
+    let newPlaylist = user.playlist.filter((playlist) => {
+        if (playlist.course.toString() !== course._id.toString())
+            return playlist
+    });
+
+    user.playlist = newPlaylist;
+    await user.save();
+    res.status(200).json({
+        success: true,
+        message: `Removed from playlsit`,
+        user
+    });
 })
